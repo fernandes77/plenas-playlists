9733919352a3f71b3d5fa0df369d073b53f8bf6c
Rodrigo Fernandes <rodrigo.fernandes@hatsu.com.br>
2020-11-15 15:28:37 -0300

    server setup

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+import logo from '../../assets/icons/logo.png';
+
+const Header = () => {
+  return (
+    <header className="page-header">
+      <div className="header-content">
+      </div>
+    </header>
+  );
+};
+
+export default Header;
